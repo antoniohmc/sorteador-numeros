0e2ac7ff0d447b06a7a00e44e9cc9bcedb1e2378
AntonioHMC <antoniomallmann01@gmail.com>
2024-07-06 13:30:08 -0300

    Conclusão ddo projeto.

--- app.js
+++ app.js
@@ -1,21 +1,62 @@
 function sortear() {
+    // Obtém o valor inserido no campo de entrada "quantidade" e converte para número inteiro
     let quantidade = parseInt(document.getElementById("quantidade").value);
+    // Obtém o valor inserido no campo de entrada "de" e converte para número inteiro
     let de = parseInt(document.getElementById("de").value);
+    // Obtém o valor inserido no campo de entrada "ate" e converte para número inteiro
     let ate = parseInt(document.getElementById("ate").value);
 
+    // Array para armazenar os números sorteados
     let sorteados = [];
     let numero;
 
+    // Laço para sortear os números
     for (let i = 0; i < quantidade; i++) {
+        // Gera um número aleatório entre 'de' e 'ate'
         numero = obterNumeroAleatorio(de, ate);
+
+        // Garante que o número sorteado ainda não foi sorteado antes
+        while (sorteados.includes(numero)) {
+            numero = obterNumeroAleatorio(de, ate);
+        }
+        // Adiciona o número sorteado ao array de sorteados
         sorteados.push(numero);
     }
 
+    // Atualiza o conteúdo do elemento com id "resultado" para mostrar os números sorteados
     let resultado = document.getElementById("resultado");
-    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
+
+    // Altera o status do botão
+    alterarStatusBotao();
 }
 
+// Função para gerar um número aleatório entre min e max
 function obterNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min)) + 1;
+}
 
+// Função para alterar o status do botão
+function alterarStatusBotao() {
+    // Obtém o botão com id "btn-reiniciar"
+    let botao = document.getElementById("btn-reiniciar");
+    // Verifica se o botão possui a classe "container__botao-desabilitado"
+    if (botao.classList.contains("container__botao-desabilitado")) {
+        // Remove a classe "container__botao-desabilitado" e adiciona a classe "container__botao"
+        botao.classList.remove("container__botao-desabilitado");
+        botao.classList.add("container__botao");
+    } else {
+        // Remove a classe "container__botao" e adiciona a classe "container__botao-desabilitado"
+        botao.classList.remove("container__botao");
+        botao.classList.add("container__botao-desabilitado");
+    }
+}
+
+// Função para reiniciar os valores dos campos de entrada
+function reiniciar() {
+    document.getElementById("quantidade").value = "";
+    document.getElementById("ate").value = "";
+    document.getElementById("de").value = "";
+    document.getElementById("resultado").innerHTML = "Números sorteados:  nenhum até agora";
+    alterarStatusBotao();
 }
